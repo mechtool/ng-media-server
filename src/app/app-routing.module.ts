cc194f11e73fcbaef432a139c56df215e1d911f2
mechtool <sovizer@gmail.com>
2021-03-25 18:09:58 +0300

    25.03

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MediaServerComponent} from "./components/media-server/media-server.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'media-server' , component : MediaServerComponent},
+  {path : '' , pathMatch : 'full', redirectTo : 'media-server'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import {MediaServerComponent} from "./components/media-server/media-server.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaServerComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
